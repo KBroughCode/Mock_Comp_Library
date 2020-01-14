036dcbe18cca608f9ed31701cf2ee8fe96a0053b
Kris Brough <a0109728@0109728s-MacBook-Pro.local>
2020-01-14 14:22:51 +0000

    Applitools and Percy both working

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,8 @@
-import { configure } from '@storybook/react';
+import { configure, getStorybook, setAddon} from '@storybook/react';
+import createPercyAddon from '@percy-io/percy-storybook';
+const { percyAddon, serializeStories } = createPercyAddon();
+setAddon(percyAddon);
 
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
+
+serializeStories(getStorybook);
---
+++ storybook-static/precache-manifest.be66ec83427a17229860677fa1651735.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37991b3f789dec62e83433a77a32dfb",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.66be116bafd48cc95d71.bundle.js"
+  },
+  {
+    "url": "runtime~main.66be116bafd48cc95d71.bundle.js"
+  },
+  {
+    "url": "vendors~main.66be116bafd48cc95d71.bundle.js"
+  },
+  {
+    "url": "vendors~main.66be116bafd48cc95d71.bundle.js.LICENSE"
+  }
+]);
